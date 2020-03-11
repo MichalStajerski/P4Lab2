676b92d52cba4b82e5b118d17684e9dd3130ddba
MichalStajerski <m918560@gmail.com>
2020-03-11 03:08:43 +0100

    dodano Lab2

---
+++ Lab_DAPPER/DB.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_DAPPER
+{
+    class DB
+    {
+        //jak nie działa using przy var 
+        //northwind properties connection string w SQL server object Explorer
+        public void Select(IDbConnection connection)
+        {
+
+            var regions = connection.Query<Region>("SELECT * FROM Region"); // nie trzeba dalej podawac ale mozna po zapytaniu
+            foreach (var item in regions)
+            {
+                Console.WriteLine($"{item.RegionId}:{item.RegionDescription}");//przez dodanie Region kolo Query pilnuje naszych nazw
+            }
+
+
+        }
+        public void Insert(IDbConnection connection, Region region)
+        {
+            connection.Execute("INSERT INTO Region(RegionID,RegionDescription)" + "VALUES(@id,@desc)",
+                new { id = region.RegionId, desc = region.RegionDescription });
+        }
+
+
+        public void Insert(IDbConnection connection, int id, string description)
+        {
+            connection.Execute("INSERT INTO Region(RegionID,RegionDescription)" + "VALUES(@id,@desc)",
+                new { id = id, desc = description });
+        }
+        public void Delete(IDbConnection connection,Region region)
+        {
+            connection.Execute("DELETE FROM  Region(RegionID, RegionDescription)" + "VALUES(@id, @desc)");
+
+        }
+
+
+
+    }
+}
---
+++ Lab_DAPPER/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.Sql;
+
+
+
+namespace Lab_DAPPER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                var db = new DB();
+                db.Select(connection);
+                db.Insert(connection, new Region() { RegionId = 300, RegionDescription = "test" });
+                //jest dodane do tabeli Region
+                db.Insert(connection, 201, "d-test");
+                //usuwanie po IdRegionu
+                db.Delete(connection, new Region(){RegionId =300 });
+            }
+
+
+
+
+
+            
+
+        }
+    }
+}
